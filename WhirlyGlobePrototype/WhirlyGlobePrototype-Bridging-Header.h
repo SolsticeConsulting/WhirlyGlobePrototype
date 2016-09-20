55e4d306596742c1bebb6653bfb63f2638a4db61
Reid Conner <rconner@solstice-consulting.com>
2016-09-20 14:31:39 -0500

    WhirlyGlobe Pod integration

---
+++ WhirlyGlobePrototype/WhirlyGlobePrototype-Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "MaplyComponent.h"
+#import "MaplyBaseViewController.h"
+#import "WhirlyGlobeViewController.h"
+#import "MaplyViewController.h"
